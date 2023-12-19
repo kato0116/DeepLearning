53ddc9be41a8938a8a4003dc30977c28f26140e1
kato0116 <j12shun@gmail.com>
2023-12-19 16:42:38 +0900

    [add]2層全結合層と畳み込み層を追加

---
+++ simple_net/simple_CNN.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 乱数固定
+def torch_seed(seed=123):   
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms = True
+
+def fit(net,optimizer,criterion,epochs,train_loader,test_loader,history,device):
+    net = net.to(device)
+    for epoch in range(epochs):
+        print(f'{epoch+1}エポック')
+        train_acc,train_loss = 0, 0
+        val_acc, val_loss    = 0, 0
+        n_train, n_test      = 0, 0
+        # 学習
+        for inputs, labels in tqdm(train_loader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            n_train += len(labels)
+            optimizer.zero_grad() # 勾配の初期化
+            outputs = net(inputs) # 予測計算
+            predicted_train = torch.max(outputs,1)[1].long()   # 予測ラベルの算出
+            
+            loss            = criterion(outputs,labels) # 損失関数
+            loss.backward()  # 勾配計算
+            optimizer.step() # パラメータの更新
+            train_loss += loss.item()
+            train_acc  += (predicted_train == labels).sum().item()
+            
+            
+        for inputs, labels in test_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            n_test += len(labels)
+            outputs_test = net(inputs)
+            loss_test    = criterion(outputs_test,labels)
+            predicted_test = torch.max(outputs_test,1)[1].long()
+            # 損失と精度の計算
+            val_loss += loss_test.item()
+            val_acc  += (predicted_test == labels).sum().item()
+
+        # 1エポックごとの平均誤差、平均精度
+        train_loss /= len(train_loader)
+        train_acc  /= n_train
+        val_loss   /= len(test_loader)
+        val_acc    /= n_test
+        print(f"train_loss:{train_loss}")
+        print(f"train_acc :{train_acc}")
+        print(f"val_loss:{val_loss}")
+        print(f"val_acc :{val_acc}")
+        history[epoch] = [epoch + 1, train_loss, train_acc, val_loss, val_acc]
+    return history    
+
+torch_seed()
+data_root = "./data" # ダウンロード先のディレクト名
+
+# データの前処理
+transform = transforms.Compose([
+    transforms.ToTensor(), # データをテンソル化処理
+    transforms.Normalize(0.5,0.5), # データの正規化 範囲を[-1,1]に変更できる
+])
+
+train_set = datasets.CIFAR10(
+    root  = data_root,
+    train = True,     # 訓練データかテストデータか
+    download  = True, # 元データがない場合ダウンロードする
+    transform = transform
+)
+test_set  = datasets.CIFAR10(
+    root  = data_root,
+    train = False,
+    download  = True,
+    transform = transform
+)
+
+batch_size = 64 # ミニバッチのサイズ指定
+# 訓練用データローダ―
+train_loader = DataLoader(
+    train_set,
+    batch_size = batch_size,
+    shuffle    = True # データにシャフルを加える
+)
+# テスト用データローダ―
+test_loader = DataLoader(
+    test_set,
+    batch_size = batch_size,
+    shuffle    = False
+)
+
+class CNN(nn.Module):
+    """
+    nn.Conv2d(チャネル数,フィルタ数,カーネルサイズ,stirde,padding)
+    nn.MaxPool2d(カーネルサイズ,stride)
+    """
+    def __init__(self,output_size,hidden_size):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1,32,3)
+        self.conv2 = nn.Conv2d(32,32,3)
+        self.relu  = nn.ReLU(inplace=True)
+        self.pool  = nn.MaxPool2d((2,2))
+        self.flatten = nn.Flatten() # 平滑化
+        self.fc1   = nn.Linear(6272,hidden_size)
+        self.fc2   = nn.Linear(hidden_size,output_size)
+        self.features = nn.Sequential(
+            self.conv1,
+            self.relu,
+            self.conv2,
+            self.relu,
+            self.pool
+        )
+        self.classifier = nn.Sequential(
+            self.fc1,
+            self.relu,
+            self.fc2
+        )
+    def forward(self,x):
+        x1 = self.features(x)
+        x2 = self.flatten(x1)
+        x3 = self.classifier(x2)
+        return x3
+
+x,l = train_set[0] 
+
+hidden_size = 128
+output_size = 10
+
+lr = 0.001
+epochs  = 2
+history = np.zeros((epochs,5)) # 記録用配列 (epoch,train_loss,train_acc,val_loss,val_acc)
+device  = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("device:",device)
+net = CNN(output_size,hidden_size)
+
+# criterion = nn.BCELoss()                      # 損失関数: 2値交差エントロピー
+criterion = nn.CrossEntropyLoss()               # 損失関数: 交差エントロピーとsoftmax関数が組み合わさっている (ミニバッチ内の平均損失が算出される)
+optimizer = optim.SGD(net.parameters(),lr = lr) # 最適化関数: 勾配降下法(SGD)
+
+history = fit(net,optimizer,criterion,epochs,train_loader,test_loader,history,device)
+
+print(f"初期状態:損失:{history[0,3]:.5f} 精度:{history[0,4]:5f}")
+print(f"最終状態:損失:{history[-1,3]:.5f} 精度:{history[-1,4]:5f}")
+
+plt.plot(history[:,0],history[:,1],c='b',label="train")
+plt.plot(history[:,0],history[:,3],c="k",label="test")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("学習曲線(損失)",fontname = 'MS Gothic')
+plt.legend()
+plt.savefig("glaph_000.png")
+plt.show()
